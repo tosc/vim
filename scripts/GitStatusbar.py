745724c25209e7b65744544caab01be20428ac6e
ToSc <tobias.schwalbe@gmail.com>
2016-05-09 15:24:03 +0200

    Defensive coding.

--- scripts/GitStatusbar.py
+++ scripts/GitStatusbar.py
@@ -8,45 +8,48 @@ import os
 import sys
 import subprocess
 
-args = sys.argv
-if len(args) > 1:
-    currentPath = args[1]
-    currentFolder,currentFile = os.path.split(currentPath)
+try:
+    args = sys.argv
+    if len(args) > 1:
+        currentPath = args[1]
+        currentFolder,currentFile = os.path.split(currentPath)
 
-    filesRaw = subprocess.check_output("git -C " + currentFolder + " status -b -s", stderr=subprocess.STDOUT)
-    rowsRaw = subprocess.check_output("git -C " + currentFolder + " diff --numstat", stderr=subprocess.STDOUT)
+        filesRaw = subprocess.check_output("git -C " + currentFolder + " status -b -s", stderr=subprocess.STDOUT)
+        rowsRaw = subprocess.check_output("git -C " + currentFolder + " diff --numstat", stderr=subprocess.STDOUT)
 
-    statusLine = ""
+        statusLine = ""
 
-    filesRaw = filesRaw.replace("...", "->")
-    filesRaw = filesRaw.replace("#", "")
-    filesSplit = filesRaw.split("\n")
-    # [master->origin/master]   Branch info
-    if len(filesSplit) > 0:
-        branch = filesSplit[0][1:].split(" ")
-        statusLine += "[" + branch[0] + "]"
-        if len(branch[1:]) > 0:
-            statusLine += " "
-            branch = branch[1:]
-            if len(branch) > 3:
-                statusLine += branch[0] + " " + branch[1][:-1] + "] ["
-                branch = branch[2:]
-            if len(branch) > 1:
-                statusLine += branch[0] + " " + branch[1]
-                branch = branch[2:]
+        filesRaw = filesRaw.replace("...", "->")
+        filesRaw = filesRaw.replace("#", "")
+        filesSplit = filesRaw.split("\n")
+        # [master->origin/master]   Branch info
+        if len(filesSplit) > 0:
+            branch = filesSplit[0][1:].split(" ")
+            statusLine += "[" + branch[0] + "]"
+            if len(branch[1:]) > 0:
+                statusLine += " "
+                branch = branch[1:]
+                if len(branch) > 3:
+                    statusLine += branch[0] + " " + branch[1][:-1] + "] ["
+                    branch = branch[2:]
+                if len(branch) > 1:
+                    statusLine += branch[0] + " " + branch[1]
+                    branch = branch[2:]
 
-    # [m 3]                     Number of modified files
-    if len(filesSplit) > 2:
-        statusLine += " [m " + str(len(filesSplit) - 2) + "]"
+        # [m 3]                     Number of modified files
+        if len(filesSplit) > 2:
+            statusLine += " [m " + str(len(filesSplit) - 2) + "]"
 
-    # [+3 -2]                   Changed rows in current file
-    for row in rowsRaw.split("\n"):
-        if currentFile in row:
-            changedRows = row.split("\t")
-            statusLine += " [+" + changedRows[0] + " -" + changedRows[1] + "]"
+        # [+3 -2]                   Changed rows in current file
+        for row in rowsRaw.split("\n"):
+            if currentFile in row:
+                changedRows = row.split("\t")
+                statusLine += " [+" + changedRows[0] + " -" + changedRows[1] + "]"
 
-    statuslineFileName = currentPath.replace("\\", "-").replace(":", "-").replace("/", "-")
+        statuslineFileName = currentPath.replace("\\", "-").replace(":", "-").replace("/", "-")
 
-    f = open(os.path.expanduser('~') + "/.vim/tmp/gitstatusline/" + statuslineFileName, 'w')
-    f.write(statusLine)
-    f.close()
+        f = open(os.path.expanduser('~') + "/.vim/tmp/gitstatusline/" + statuslineFileName, 'w')
+        f.write(statusLine)
+        f.close()
+except:
+    print "Failed to update Statusbar"
